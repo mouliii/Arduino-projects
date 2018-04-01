28cf11230b1bd1ece130954405b03c83c85b82ef
Aleksi Räsänen <rasanen.aleksi@hotmail.com>
2018-04-01 14:17:27 +0300

    added gyro and pid

---
+++ Reveiver/Reveiver/Gyro.h
@@ -0,0 +1,41 @@
+// Gyro.h
+
+#ifndef _GYRO_h
+#define _GYRO_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+class Gyro
+{
+ public:
+	 Gyro();
+	 void setup_mpu_6050_registers();
+	 void Init();
+	 void read_mpu_6050_data();
+	 float anglePitch()
+	 {
+		 return angle_pitch;
+	 }
+	 float angleRoll()
+	 {
+		 return angle_roll;
+	 }
+private:
+	int gyro_x, gyro_y, gyro_z;
+	long acc_x, acc_y, acc_z, acc_total_vector;
+	int temperature;
+	long gyro_x_cal, gyro_y_cal, gyro_z_cal;
+	int lcd_loop_counter;
+	float angle_pitch, angle_roll;
+	int angle_pitch_buffer, angle_roll_buffer;
+	float angle_roll_acc, angle_pitch_acc;
+	float angle_pitch_output, angle_roll_output;
+	bool set_gyro_angles = false;
+};
+
+#endif
+
--- Reveiver/Reveiver/Reveiver.ino
+++ Reveiver/Reveiver/Reveiver.ino
@@ -1,22 +1,29 @@
-// SimpleRx - the slave or the receiver
+// TODO SHITTII TAI MUISTUTKSIA
+// 
+// TODO YAW PID
+// MOOTTORI()
+// 
+///////////////////////////////////////////////
 
+
+#include "Gyro.h"
+#include <Wire.h>
 #include <Servo.h>
 #include <SPI.h>
-#include <nRF24L01.h>
 #include <RF24.h>
 
 #define CE_PIN   9
 #define CSN_PIN 10
 
 const byte thisSlaveAddress = 76;
-const byte maxAngle = 20;
+const char maxAngle = 20;
 
 RF24 radio(CE_PIN, CSN_PIN);
-Servo esc1;
+Servo esc1;	
 Servo esc2;
 Servo esc3;
 Servo esc4;
-
+Gyro gyro;
 bool newData = false; // <- debug
 
 struct Input
@@ -26,14 +33,30 @@ struct Input
 	int roll = 0;
 	int yaw = 0;
 };
-
 Input inputs;
 
-//===========
+long loop_timer;
+//////////////// PID CONSTANTS ////////////////
+const float kp = 0.0f;
+const float ki = 0.0f;
+const float kd = 0.0f;
+//////////////// //////////// ////////////////
+float pid_p = 0.0f;
+float pid_i = 0.0f;
+float pid_d = 0.0f;
+float error = 0.0f; 
+float prevError = 0.0f;
+float pid_roll = 0.0f;
+float pid_pitch = 0.0f;
+float pid_yaw = 0.0f;
+//////////////////////////////////////////////
 
 void setup() {
 
 	Serial.begin(9600);
+	// gyro
+	Wire.begin();
+	gyro.setup_mpu_6050_registers();
 	// radio
 	Serial.println("SimpleRx Starting");
 	radio.begin();
@@ -43,22 +66,37 @@ void setup() {
 	radio.openReadingPipe(1, thisSlaveAddress);
 	radio.startListening();
 	// esc
-	esc1.attach(2);
-	esc2.attach(3);
-	esc3.attach(4);
-	esc4.attach(5);
+	esc1.attach(2); // top left
+	esc2.attach(3); // top right
+	esc3.attach(4); // bottom left
+	esc4.attach(5); // bottom right
+
+	//Reset the loop timer
+	loop_timer = micros();
 }
 
 //=============
 
 void loop() {
-	getData();
+	GetData();
+	GetGyroData();
+	CalculatePID();
+	WriteToMotors();
 	showData(); // <- debug
+	if (micros() - loop_timer > 4000)
+	{
+		Serial.println("loop timer too high!!!!");
+	}
+
+	while (micros() - loop_timer < 4000);                                //Wait until the loop_timer reaches 4000us (250Hz) before starting the next loop
+	{
+		loop_timer = micros();                                               //Reset the loop timer
+	}
 }
 
 //==============
 
-void getData() {
+void GetData() {
 	if (radio.available()) {
 		radio.read(&inputs, sizeof(inputs));
 		// joystick raw input zeroed //
@@ -68,34 +106,84 @@ void getData() {
 		// mapping
 		inputs.roll = map(inputs.roll, -522, 501, -maxAngle, maxAngle);
 		inputs.pitch = map(inputs.pitch, -525, 498, -maxAngle, maxAngle);
-		//inputs.thrust = map(inputs.thrust, 0, 1000, 0, 179);   /// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<< ///
-		// half values
-		int rollHalf = inputs.roll / 2;
-		int pitchHalf = inputs.pitch / 2;
 
-		Serial.println(rollHalf);
-		Serial.println(pitchHalf);
-
-		// calculate and write motors true values
-		if (inputs.thrust + 1000 > 1940)
-		{
-			inputs.thrust = 850;
-		}
-		esc1.writeMicroseconds(inputs.thrust + 1000 + rollHalf + pitchHalf);
-		//Serial.print("m1: ");  Serial.println(inputs.thrust + 1000 + rollHalf + pitchHalf);
-
-		esc2.writeMicroseconds(inputs.thrust + 1000 - rollHalf + pitchHalf);
-		//Serial.print("m2: ");  Serial.println(inputs.thrust + 1000 - rollHalf + pitchHalf);
-
-		esc3.writeMicroseconds(inputs.thrust + 1000 + rollHalf - pitchHalf);
-		//Serial.print("m3: ");  Serial.println(inputs.thrust + 1000 + rollHalf - pitchHalf);
-
-		esc4.writeMicroseconds(inputs.thrust + 1000 - rollHalf - pitchHalf);
-		//Serial.print("m4: ");  Serial.println(inputs.thrust + 1000 - rollHalf - pitchHalf);
-		
 		newData = true; // <- debug
 	}
 }
+
+void GetGyroData()
+{
+	gyro.read_mpu_6050_data();
+}
+
+void CalculatePID()
+{
+	// ROLL ///////////////////////////////
+	error = gyro.angleRoll() - inputs.roll;
+	// kp
+	pid_p = kp * error;
+	// ki and limit checks
+	pid_i += ki * error;
+	if (pid_i > maxAngle)
+	{
+		pid_i = maxAngle;
+	}
+	else if (pid_i < -maxAngle)
+	{
+		pid_i = -maxAngle;
+	}
+	// kd
+	pid_d = kd * (error - prevError);
+	// total roll pid
+	pid_roll = pid_p + pid_i + pid_d;
+	// total limit check
+	if (pid_roll > maxAngle)
+	{
+		pid_roll = maxAngle;
+	}
+	else if (pid_roll < -maxAngle)
+	{
+		pid_roll = -maxAngle;
+	}
+
+	// PITCH  ///////////////////////////////
+	error = gyro.anglePitch() - inputs.pitch;
+	// kp
+	pid_p = kp * error;
+	// ki and limit checks
+	pid_i += ki * error;
+	if (pid_i > maxAngle)
+	{
+		pid_i = maxAngle;
+	}
+	else if (pid_i < -maxAngle)
+	{
+		pid_i = -maxAngle;
+	}
+	// kd
+	pid_d = kd * (error - prevError);
+	// total roll pid
+	pid_pitch = pid_p + pid_i + pid_d;
+	// total limit check
+	if (pid_pitch > maxAngle)
+	{
+		pid_pitch = maxAngle;
+	}
+	else if (pid_pitch < -maxAngle)
+	{
+		pid_pitch = -maxAngle;
+	}
+	// YAW  /////////////////////////////////
+
+	// TODO
+	prevError = error;
+}
+
+void WriteToMotors()
+{
+
+}
+
 
 void showData() {
 	if (newData == true) {
