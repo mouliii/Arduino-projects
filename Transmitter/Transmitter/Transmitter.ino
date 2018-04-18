db22fcf4fe9d982cf996c56360352bc42bd57077
Aleksi Räsänen <rasanen.aleksi@hotmail.com>
2018-04-18 17:18:38 +0300

    added yaw controller test

--- Transmitter/Transmitter/Transmitter.ino
+++ Transmitter/Transmitter/Transmitter.ino
@@ -1,8 +1,13 @@
 // SimpleTx - the master or the transmitter
 #include <SPI.h>
-#include <VirtualWire/VirtualWire.h>
+#include <RF24.h>
 
+#define CE_PIN   9
+#define CSN_PIN 10
 
+const byte slaveAddress = 76;
+
+RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
 
 unsigned long currentMillis;
 unsigned long prevMillis;
@@ -18,16 +23,21 @@ struct Input
 
 Input inputs;
 
-
 void setup() {
 
 	Serial.begin(9600);
 
 	Serial.println("SimpleTx Starting");
 
-	vw_set_tx_pin(7);
-	vw_setup(2000);
-	vw_set_ptt_inverted(true);
+	pinMode(2, INPUT_PULLUP); // right
+	pinMode(3, INPUT_PULLUP); // left
+
+	radio.begin();
+	radio.setDataRate(RF24_250KBPS);
+	radio.setRetries(3, 5); // delay, count
+	radio.setAutoAck(false);
+	radio.setPALevel(RF24_PA_MAX);
+	radio.openWritingPipe(slaveAddress);
 }
 
 //====================
@@ -36,8 +46,17 @@ void loop() {
 	currentMillis = millis();
 	if (currentMillis - prevMillis >= txIntervalMillis) {
 		inputs.thrust = analogRead(A0);
-		inputs.pitch = analogRead(A4);
-		inputs.roll = analogRead(A6);
+		inputs.pitch = analogRead(A1);
+		inputs.roll = analogRead(A3);
+		if (digitalRead(2) == LOW)
+		{
+			inputs.yaw = 1;
+		}
+		else if (digitalRead(3) == LOW)
+		{
+			inputs.yaw = -1;
+		}
+
 		send();
 		prevMillis = millis();
 	}
@@ -47,6 +66,26 @@ void loop() {
 
 void send() {
 
-	vw_send((uint8_t*)&inputs, sizeof(inputs));
-	Serial.println(inputs.thrust);
+	radio.write(&inputs, sizeof(inputs));
+	// Always use sizeof() as it gives the size as the number of bytes.
+	// For example if dataToSend was an int sizeof() would correctly return 2
+	//Serial.println(analogRead(A3));
 }
+
+/*
+throttle
+0 - 674
+100 - 1023
+*/
+/*
+roll
+482 middle
+vas 1023
+oik 0
+*/
+/*
+pitch
+483 middle
+1023 ala
+0 up
+*/
