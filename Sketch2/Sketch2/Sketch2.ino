9ea14920f05b2096ac0fc0ccf10611d0c83acef5
Aleksi Räsänen <rasanen.aleksi@hotmail.com>
2018-10-04 18:42:22 +0300

    lähetin 7 segmenttiin

--- Sketch2/Sketch2/Sketch2.ino
+++ Sketch2/Sketch2/Sketch2.ino
@@ -1,24 +1,25 @@
 #include <OneWire.h>
 #include <DallasTemp/DallasTemperature.h>
-#include "virtualwire/VirtualWire.h"
+#include "arduino-libs-manchester-master/Manchester.h"
 
-#define ONE_WIRE_BUS 7
+#define ONE_WIRE_BUS 8
+#define TX_PIN 7 //pin where your transmitter is connected
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-float temp = 0;
+float temp = 0.0f;
 float currentTime = 0.0f;
+uint8_t data[5] = { 4, 0, 0, 0, 0 };
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 	Serial.begin(9600);
-	sensors.begin();
 	sensors.setResolution(12);
+	sensors.begin();
 
-	vw_set_tx_pin(6);
-	vw_set_ptt_inverted(true); // Required for DR3100
-	vw_setup(2000);	 // Bits per sec
+	//man.workAround1MhzTinyCore(); //add this in order for transmitter to work with 1Mhz Attiny85/84
+	man.setupTransmit(TX_PIN, MAN_1200);
 
 	currentTime = millis();
 }
@@ -26,14 +27,45 @@ void setup() {
 // the loop function runs over and over again until power down or reset
 void loop() {
 
-	if (millis() - currentTime > 2500)
+	if (millis() - currentTime > 3000)
 	{
-		sensors.setResolution(12);
-		sensors.setWaitForConversion(true);
+		//read temp
 		sensors.requestTemperatures();
 		temp = sensors.getTempCByIndex(0);
-		vw_send((uint8_t*) &temp, sizeof(temp));
+		// check if negative
+		if (temp < 0.0f)
+		{
+			temp += 100.0f;
+			data[3] = 1;
+		}
+		else
+		{
+			data[3] = 0;
+		}
+		// limits
+		float decs = temp;
+		fmod(decs, 10);
+		decs * 1000;
+		fmod(decs, 10);
+		if (decs > 5.0f)
+		{
+			temp += 0.01f;
+		}
+		// math
+		data[1] = (int)temp;
+		data[2] = (int)fmod(temp*100,100);
+		for (int i = 0; i < 2; i++)
+		{
+			Serial.println(data[i+1]);
+		}
+		Serial.println(data[1] + data[2] / 100.f);
+		// send
+		for (size_t i = 0; i < 1; i++)
+		{
+			man.transmitArray(sizeof(data), data);
+			delay(200);
+		}
+		
 		currentTime = millis();
-		Serial.println(temp);
 	}
 }
