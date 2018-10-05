f8771388ec2c54170da80d597a21ebd904419cd3
Aleksi Räsänen <rasanen.aleksi@hotmail.com>
2018-10-05 12:30:08 +0300

    virtualwire

--- 7 segment/7 segment/7 segment.ino
+++ 7 segment/7 segment/7 segment.ino
@@ -1,22 +1,23 @@
 #include <OneWire-master/OneWire.h>
 #include "SevSeg-master/SevSeg.h"
-#include "DallasTemp/DallasTemperature.h"
-#include <arduino-libs-manchester-master/Manchester.h>
+#include <VirtualWire.h>
 
 #define RX_PIN 10 //pin where your transmitter is connected
 
 SevSeg sevseg;
 
 float temp = 0.0f;
-uint8_t data[5] = { 4, 0, 0, 0, 0 };
-float c1 = 0.0f;
-float c2 = 0.0f;
+uint8_t len = sizeof(temp);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 	Serial.begin(9600);
-	man.setupReceive(RX_PIN, MAN_1200);
-	man.beginReceiveArray(sizeof(data), data);
+	// Initialise the IO and ISR
+	vw_set_ptt_inverted(true); // Required for DR3100
+	vw_set_rx_pin(10);
+	vw_setup(2000); // Bits per sec
+	vw_rx_start(); // Start the receiver PLL running
+	// screen
 	byte numDigits = 4;
 	byte digitPins[] = { A0, A1, A2, A3 };
 	byte segmentPins[] = { 2, 3, 4, 5, 6, 7, 8, 9 };
@@ -28,40 +29,68 @@ void setup() {
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-	if (man.receiveComplete()) {
-		Serial.println("msg");
-		Serial.println(data[0]);
-		Serial.println(data[1]);
-		Serial.println(data[2]);
-		uint8_t receivedSize = data[0];
-		//do something with the data in 'buffer' here before you start receiving to the same buffer again
-		c1 = data[1];
-		c2 = data[2];
-		if (data[3] == 1)
+	
+	if (vw_get_message((uint8_t *)&temp, &len))
+	{
+		Serial.println(temp);
+		if (temp > 0.0f)
 		{
-			c1 -= 100;
+			sevseg.setNumber(temp, 2);
 		}
-		float c3 = c1 + c2 / 100.f;
-		Serial.println(c3);
-		if (c3 > 0.0f)
-		{
-			sevseg.setNumber(c3, 2);
-		}
-		else if (c3 < 0.0f && c3 > -10.0f)
+		else if (temp < 0.0f && temp > -10.0f)
 		{
 			//offset leading zeroes ?
-			sevseg.setNumber(c3, 2);
+			sevseg.setNumber(temp, 2);
 			sevseg.setChars("-");
 		}
-		else if (c3 < -10.0f)
+		else if (temp < -10.0f)
 		{
 			//offset ^ ^ ^ ^ ?
-			sevseg.setNumber(c3, 1);
+			sevseg.setNumber(temp, 1);
 			sevseg.setChars("-");
 		}
-		
-		man.beginReceiveArray(sizeof(data), data);
 	}
 
 	sevseg.refreshDisplay();
 }
+
+
+
+/*
+
+if (man.receiveComplete()) {
+Serial.println("msg");
+Serial.println(data[0]);
+Serial.println(data[1]);
+Serial.println(data[2]);
+uint8_t receivedSize = data[0];
+//do something with the data in 'buffer' here before you start receiving to the same buffer again
+c1 = data[1];
+c2 = data[2];
+if (data[3] == 1)
+{
+c1 -= 100;
+}
+float c3 = c1 + c2 / 100.f;
+Serial.println(c3);
+if (c3 > 0.0f)
+{
+sevseg.setNumber(c3, 2);
+}
+else if (c3 < 0.0f && c3 > -10.0f)
+{
+//offset leading zeroes ?
+sevseg.setNumber(c3, 2);
+sevseg.setChars("-");
+}
+else if (c3 < -10.0f)
+{
+//offset ^ ^ ^ ^ ?
+sevseg.setNumber(c3, 1);
+sevseg.setChars("-");
+}
+
+man.beginReceiveArray(sizeof(data), data);
+}
+
+*/
--- Sketch2/Sketch2/Sketch2.ino
+++ Sketch2/Sketch2/Sketch2.ino
@@ -1,6 +1,6 @@
 #include <OneWire.h>
 #include <DallasTemp/DallasTemperature.h>
-#include "arduino-libs-manchester-master/Manchester.h"
+#include <VirtualWire.h>
 
 #define ONE_WIRE_BUS 8
 #define TX_PIN 7 //pin where your transmitter is connected
@@ -10,16 +10,17 @@ DallasTemperature sensors(&oneWire);
 
 float temp = 0.0f;
 float currentTime = 0.0f;
-uint8_t data[5] = { 4, 0, 0, 0, 0 };
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 	Serial.begin(9600);
+	//temp sensor
 	sensors.setResolution(12);
 	sensors.begin();
-
-	//man.workAround1MhzTinyCore(); //add this in order for transmitter to work with 1Mhz Attiny85/84
-	man.setupTransmit(TX_PIN, MAN_1200);
+	// radio
+	vw_set_ptt_inverted(true); // Required for DR3100
+	vw_set_tx_pin(7);
+	vw_setup(2000); // Bits per sec
 
 	currentTime = millis();
 }
@@ -32,40 +33,53 @@ void loop() {
 		//read temp
 		sensors.requestTemperatures();
 		temp = sensors.getTempCByIndex(0);
-		// check if negative
-		if (temp < 0.0f)
-		{
-			temp += 100.0f;
-			data[3] = 1;
-		}
-		else
-		{
-			data[3] = 0;
-		}
-		// limits
-		float decs = temp;
-		fmod(decs, 10);
-		decs * 1000;
-		fmod(decs, 10);
-		if (decs > 5.0f)
-		{
-			temp += 0.01f;
-		}
-		// math
-		data[1] = (int)temp;
-		data[2] = (int)fmod(temp*100,100);
-		for (int i = 0; i < 2; i++)
-		{
-			Serial.println(data[i+1]);
-		}
-		Serial.println(data[1] + data[2] / 100.f);
-		// send
-		for (size_t i = 0; i < 1; i++)
-		{
-			man.transmitArray(sizeof(data), data);
-			delay(200);
-		}
-		
+		Serial.println(temp);
+		//send
+		vw_send((uint8_t *)&temp, sizeof(temp));
+		//reset timer
 		currentTime = millis();
 	}
 }
+
+// manchester coding code
+/*
+
+//read temp
+sensors.requestTemperatures();
+temp = sensors.getTempCByIndex(0);
+// check if negative
+if (temp < 0.0f)
+{
+temp += 100.0f;
+data[3] = 1;
+}
+else
+{
+data[3] = 0;
+}
+// limits
+float decs = temp;
+fmod(decs, 10);
+decs * 1000;
+fmod(decs, 10);
+if (decs > 5.0f)
+{
+temp += 0.01f;
+}
+// math
+data[1] = (int)temp;
+data[2] = (int)fmod(temp*100,100);
+for (int i = 0; i < 2; i++)
+{
+Serial.println(data[i+1]);
+}
+Serial.println(data[1] + data[2] / 100.f);
+// send
+for (size_t i = 0; i < 1; i++)
+{
+man.transmitArray(sizeof(data), data);
+delay(200);
+}
+
+
+*/
